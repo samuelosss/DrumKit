2fab1e4f46ad5b3f57bf33c35872aef524bf5c0c
Sam Poloha <samuel.poloha@gmail.com>
2023-08-10 13:32:22 +0200

    repair to nice version - Angela's solution

--- index.js
+++ index.js
@@ -194,48 +194,48 @@
 // IT WORKS!! no clue why the error appeared before. But doing it other way now with first listener
 //as querySelectorAll misto getElementsClassName jako predtim (a ted stejne definovana class .drum)
 
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-	document
-		.getElementsByClassName("drum")
-		[i].addEventListener("click", function () {
-			var buttonInnerHTML = this.innerHTML;
+// for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+// 	document
+// 		.getElementsByClassName("drum")
+// 		[i].addEventListener("click", function () {
+// 			var buttonInnerHTML = this.innerHTML;
 
-			switch (buttonInnerHTML) {
-				case "w":
-					var tom1 = new Audio("./sounds/tom-1.mp3");
-					tom1.play();
-					break;
-				case "a":
-					var tom2 = new Audio("./sounds/tom-2.mp3");
-					tom2.play();
-					break;
-				case "s":
-					var tom3 = new Audio("./sounds/tom-3.mp3");
-					tom3.play();
-					break;
-				case "d":
-					var tom4 = new Audio("./sounds/tom-4.mp3");
-					tom4.play();
-					break;
-				case "j":
-					var snare = new Audio("./sounds/snare.mp3");
-					snare.play();
-					break;
-				case "k":
-					var kick = new Audio("./sounds/kick-bass.mp3");
-					kick.play();
-					break;
-				case "l":
-					var crash = new Audio("./sounds/crash.mp3");
-					crash.play();
-					break;
+// 			switch (buttonInnerHTML) {
+// 				case "w":
+// 					var tom1 = new Audio("./sounds/tom-1.mp3");
+// 					tom1.play();
+// 					break;
+// 				case "a":
+// 					var tom2 = new Audio("./sounds/tom-2.mp3");
+// 					tom2.play();
+// 					break;
+// 				case "s":
+// 					var tom3 = new Audio("./sounds/tom-3.mp3");
+// 					tom3.play();
+// 					break;
+// 				case "d":
+// 					var tom4 = new Audio("./sounds/tom-4.mp3");
+// 					tom4.play();
+// 					break;
+// 				case "j":
+// 					var snare = new Audio("./sounds/snare.mp3");
+// 					snare.play();
+// 					break;
+// 				case "k":
+// 					var kick = new Audio("./sounds/kick-bass.mp3");
+// 					kick.play();
+// 					break;
+// 				case "l":
+// 					var crash = new Audio("./sounds/crash.mp3");
+// 					crash.play();
+// 					break;
 
-				default:
-					console.log(buttonInnerHTML);
-					break;
-			}
-		});
-}
+// 				default:
+// 					console.log(buttonInnerHTML);
+// 					break;
+// 			}
+// 		});
+// }
 
 // document.addEventListener("keydown", function (something) {
 //     console.log(something); //checking what key was pressed
@@ -330,3 +330,57 @@ document.addEventListener("keydown", function (whichKey) {
 });
 
 //YEP that works!
+
+// but let's comb it so it's easier to read just as Angela has it
+
+//Check for button clicks
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+	document
+		.getElementsByClassName("drum")
+		[i].addEventListener("click", function () {
+			var buttonInnerHTML = this.innerHTML;
+			makeSound(buttonInnerHTML);
+		});
+}
+
+//Check for keypad strokes
+document.addEventListener("keydown", function (event) {
+	makeSound(event.key);
+});
+
+function makeSound(key) {
+	switch (key) {
+		case "w":
+			var tom1 = new Audio("./sounds/tom-1.mp3");
+			tom1.play();
+			break;
+		case "a":
+			var tom2 = new Audio("./sounds/tom-2.mp3");
+			tom2.play();
+			break;
+		case "s":
+			var tom3 = new Audio("./sounds/tom-3.mp3");
+			tom3.play();
+			break;
+		case "d":
+			var tom4 = new Audio("./sounds/tom-4.mp3");
+			tom4.play();
+			break;
+		case "j":
+			var snare = new Audio("./sounds/snare.mp3");
+			snare.play();
+			break;
+		case "k":
+			var kick = new Audio("./sounds/kick-bass.mp3");
+			kick.play();
+			break;
+		case "l":
+			var crash = new Audio("./sounds/crash.mp3");
+			crash.play();
+			break;
+
+		default:
+			console.log("Don't break that keyboard! :)");
+			break;
+	}
+}
