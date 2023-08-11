53069129e3e2de88ee8b8a3a97b9e9a63e275883
Sam Poloha <samuel.poloha@gmail.com>
2023-08-11 09:00:57 +0200

    some small playaround

--- index.js
+++ index.js
@@ -290,64 +290,65 @@
 
 //NOPE.. trying to put inside the event listener
 
-document.addEventListener("keydown", function (whichKey) {
-	var keyName = whichKey.key;
+// document.addEventListener("keydown", function (whichKey) {
+// 	var keyName = whichKey.key;
 
-	switch (keyName) {
-		case "w":
-			var tom1 = new Audio("./sounds/tom-1.mp3");
-			tom1.play();
-			break;
-		case "a":
-			var tom2 = new Audio("./sounds/tom-2.mp3");
-			tom2.play();
-			break;
-		case "s":
-			var tom3 = new Audio("./sounds/tom-3.mp3");
-			tom3.play();
-			break;
-		case "d":
-			var tom4 = new Audio("./sounds/tom-4.mp3");
-			tom4.play();
-			break;
-		case "j":
-			var snare = new Audio("./sounds/snare.mp3");
-			snare.play();
-			break;
-		case "k":
-			var kick = new Audio("./sounds/kick-bass.mp3");
-			kick.play();
-			break;
-		case "l":
-			var crash = new Audio("./sounds/crash.mp3");
-			crash.play();
-			break;
+// 	switch (keyName) {
+// 		case "w":
+// 			var tom1 = new Audio("./sounds/tom-1.mp3");
+// 			tom1.play();
+// 			break;
+// 		case "a":
+// 			var tom2 = new Audio("./sounds/tom-2.mp3");
+// 			tom2.play();
+// 			break;
+// 		case "s":
+// 			var tom3 = new Audio("./sounds/tom-3.mp3");
+// 			tom3.play();
+// 			break;
+// 		case "d":
+// 			var tom4 = new Audio("./sounds/tom-4.mp3");
+// 			tom4.play();
+// 			break;
+// 		case "j":
+// 			var snare = new Audio("./sounds/snare.mp3");
+// 			snare.play();
+// 			break;
+// 		case "k":
+// 			var kick = new Audio("./sounds/kick-bass.mp3");
+// 			kick.play();
+// 			break;
+// 		case "l":
+// 			var crash = new Audio("./sounds/crash.mp3");
+// 			crash.play();
+// 			break;
 
-		default:
-			console.log("Don't break that keyboard! :)");
-			break;
-	}
-});
+// 		default:
+// 			console.log("Don't break that keyboard! :)");
+// 			break;
+// 	}
+// });
 
 //YEP that works!
 
 // but let's comb it so it's easier to read just as Angela has it
 
 //Check for button clicks
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-	document
-		.getElementsByClassName("drum")
-		[i].addEventListener("click", function () {
-			var buttonInnerHTML = this.innerHTML;
-			makeSound(buttonInnerHTML);
-		});
-}
+// for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+// 	document
+// 		.getElementsByClassName("drum")
+// 		[i].addEventListener("click", function () {
+// 			var buttonInnerHTML = this.innerHTML;
+// 			makeSound(buttonInnerHTML);
+// 		});
+// }
 
 //Check for keypad strokes
-document.addEventListener("keydown", function (event) {
-	makeSound(event.key);
-});
+// document.addEventListener("keydown", function (event) {
+// 	makeSound(event.key);
+// });
 
+//play sounds
 function makeSound(key) {
 	switch (key) {
 		case "w":
@@ -383,4 +384,30 @@ function makeSound(key) {
 			console.log("Don't break that keyboard! :)");
 			break;
 	}
+}
+
+//adding animation:
+
+// button click and new method buttonAnimation
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+		var buttonInnerHTML = this.innerHTML;
+		makeSound(buttonInnerHTML);
+		buttonAnimation(buttonInnerHTML);
+	});
+}
+
+//key press and new method buttonAnimation
+document.addEventListener("keydown", function (event) {
+	makeSound(event.key);
+	buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey) {
+	var activeButton = document.querySelector("." + currentKey);
+	activeButton.classList.add("pressed");
+
+	setTimeout(function () {
+		activeButton.classList.remove("pressed");
+	}, 100);
 }
